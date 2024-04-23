b131918dfda80fc7f166a200bd3d7df471d6cb18
Daniel Bahrami <daniel@bahrami.dk>
2024-04-23 23:34:23 +0200

    Configure ADC

--- temperature-publication/src/main.rs
+++ temperature-publication/src/main.rs
@@ -1,19 +1,20 @@
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::adc::*;
+use esp_idf_hal::adc::config::Config;
+use esp_idf_hal::sys::adc_atten_t_ADC_ATTEN_DB_12;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
-
-    println!("Hello World!");
-
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+
+    let mut adc = AdcDriver::new(peripherals.adc2, &Config::new()).unwrap();
+    let mut adc_pin: esp_idf_hal::adc::AdcChannelDriver<'_, {adc_atten_t_ADC_ATTEN_DB_12}, Gpio4> = AdcChannelDriver::new(peripherals.pins.gpio4).unwrap();
 
     loop {
-        let _ = led.set_high();
-        FreeRtos::delay_ms(1000);
-        let _ = led.set_low();
+        let sample = adc.read(&mut adc_pin).unwrap();
+        println!("{}", sample);
         FreeRtos::delay_ms(1000);
     }
 }
