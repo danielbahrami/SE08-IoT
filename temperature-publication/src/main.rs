321b4dd0845cb0810a81239a51444d35b53ce2d9
Daniel Bahrami <daniel@bahrami.dk>
2024-04-23 18:28:12 +0200

    Test environment

--- temperature-publication/src/main.rs
+++ temperature-publication/src/main.rs
@@ -1,10 +1,19 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+
+fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    println!("Hello World!");
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+
+    loop {
+        let _ = led.set_high();
+        FreeRtos::delay_ms(1000);
+        let _ = led.set_low();
+        FreeRtos::delay_ms(1000);
+    }
 }
