5619324367a85071546e2494d0e138df16f70f7e
Daniel Bahrami <daniel@bahrami.dk>
2024-04-25 21:20:48 +0200

    Update ADC

--- temperature-publication/src/main.rs
+++ temperature-publication/src/main.rs
@@ -1,20 +1,22 @@
-use esp_idf_hal::delay::FreeRtos;
-use esp_idf_hal::gpio::*;
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::adc::*;
+use std::thread;
+use std::time::Duration;
+
 use esp_idf_hal::adc::config::Config;
-use esp_idf_hal::sys::adc_atten_t_ADC_ATTEN_DB_12;
+use esp_idf_hal::adc::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_sys::{self as _};
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = Peripherals::take()?;
 
-    let mut adc = AdcDriver::new(peripherals.adc2, &Config::new()).unwrap();
-    let mut adc_pin: esp_idf_hal::adc::AdcChannelDriver<'_, {adc_atten_t_ADC_ATTEN_DB_12}, Gpio4> = AdcChannelDriver::new(peripherals.pins.gpio4).unwrap();
+    let mut adc = AdcDriver::new(peripherals.adc2, &Config::new().calibration(true))?;
+
+    let mut adc_pin: esp_idf_hal::adc::AdcChannelDriver<{ attenuation::DB_11 }, _> =
+    AdcChannelDriver::new(peripherals.pins.gpio12)?;
 
     loop {
-        let sample = adc.read(&mut adc_pin).unwrap();
-        println!("{}", sample);
-        FreeRtos::delay_ms(1000);
+        thread::sleep(Duration::from_millis(1000));
+        println!("ADC value: {}", adc.read(&mut adc_pin)?);
     }
 }
