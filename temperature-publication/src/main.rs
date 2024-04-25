ac51a6c9247755112384d387c51dbeec05b73eaf
Daniel Bahrami <daniel@bahrami.dk>
2024-04-25 22:42:56 +0200

    Set up wifi functionality

--- temperature-publication/src/main.rs
+++ temperature-publication/src/main.rs
@@ -4,10 +4,14 @@ use std::time::Duration;
 use esp_idf_hal::adc::config::Config;
 use esp_idf_hal::adc::*;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::{BlockingWifi, EspWifi}};
 use esp_idf_sys::{self as _};
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 
 const V_MAX: u32 = 2450;
 const D_MAX: u32 = 4095;
+const SSID: &str = "";
+const PASSWORD: &str = "";
 
 fn calculate_v_out(d_out: f32, v_max: f32, d_max: f32) -> f32 {
    d_out * (v_max / d_max)
@@ -15,6 +19,30 @@ fn calculate_v_out(d_out: f32, v_max: f32, d_max: f32) -> f32 {
 
 fn calculate_temperature() {
 
+}
+
+fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    println!("Wifi started");
+
+    wifi.connect()?;
+    println!("Wifi connected");
+
+    wifi.wait_netif_up()?;
+    println!("Wifi netif up");
+
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
@@ -25,6 +53,13 @@ fn main() -> anyhow::Result<()> {
 
     let mut adc_pin: esp_idf_hal::adc::AdcChannelDriver<{ attenuation::DB_11 }, _> =
     AdcChannelDriver::new(peripherals.pins.gpio12)?;
+
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+    let mut wifi = BlockingWifi::wrap(EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?, sys_loop,)?;
+    connect_wifi(&mut wifi)?;
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    println!("Wifi DHCP info: {:?}", ip_info);
 
     loop {
         let d_out = adc.read(&mut adc_pin)?;
